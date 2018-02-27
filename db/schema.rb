aee9d2e98d916744b1880499cdb711c5c048080d
Philippe Kerckove <Philippe.kerckove@fr.atlascopco.com>
2018-02-27 11:50:44 +0100

    DB add booking user fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226153729) do
+ActiveRecord::Schema.define(version: 20180227101915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date_from"
+    t.date "date_to"
+    t.text "comment"
+    t.boolean "status"
+    t.bigint "user_id"
+    t.bigint "horse_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["horse_id"], name: "index_bookings_on_horse_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "horses", force: :cascade do |t|
     t.string "name"
@@ -46,9 +59,18 @@ ActiveRecord::Schema.define(version: 20180226153729) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "telephone"
+    t.string "niveau"
+    t.string "licence"
+    t.string "assurance"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "horses"
+  add_foreign_key "bookings", "users"
   add_foreign_key "horses", "users"
 end
