4cc34586e92fc6e74d7d93cb246d85070449be74
Lucie Moreau <luciem.moreau@gmail.com>
2018-02-27 11:48:32 +0100

    horses views

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,14 +1,16 @@
 class HorsesController < ApplicationController
+  before_action :set_horse, only: [:show, :edit, :update, :destroy]
+
   def index
     @horses = Horse.all
   end
 
   def show
-    @horse = Horse.find(params[:id])
   end
 
   def create
     @horse = Horse.new(horse_params)
+    @horse.user = current_user
     if @horse.save
       redirect_to horse_path(@horse)
     else
@@ -20,10 +22,24 @@ class HorsesController < ApplicationController
     @horse = Horse.new
   end
 
+  def edit
+  end
+
+  def update
+    @horse.update(horse_params)
+    redirect_to horses_path
+  end
+
   def destroy
+    @horse.destroy
+    redirect_to horses_path
   end
 
   private
+
+  def set_horse
+    @horse = Horse.find(params[:id])
+  end
 
   def horse_params
     params.require(:horse).permit(:name, :coat, :age, :gender, :breed, :localisation, :price, :category, :description, :size, :photo, :photo_cache)
